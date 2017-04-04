08acd4042ce9196f114c3e29ce65ccd657c48c81
imears <maxwellmears@gmail.com>
2017-04-04 08:57:38 -0700

    Add CC info correctly

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const inquirer = require('inquirer');
 require('dotenv').config();
 
 const address = new dominos.Address({
+  Type: 'Business',
   Street: process.env.STREET,
   City: process.env.CITY,
   Region: process.env.REGION,
-  PostalCode: process.env.POSTAL_CODE
+  PostalCode: process.env.ADDRESS_POSTAL_CODE
 });
 
 const customer = new dominos.Customer({
@@ -15,45 +16,54 @@ const customer = new dominos.Customer({
   lastName: process.env.LAST_NAME,
   address: address,
   email: process.env.EMAIL,
-  phone: parseInt(process.env.PHONE)
+  phone: process.env.PHONE
 });
 
 const order = new dominos.Order({
   customer: customer,
-  storeID: parseInt(process.env.STORE_ID),
+  storeID: process.env.STORE_ID,
   deliveryMethod: 'Delivery'
 });
 
 const pizza = new dominos.Item({
-  code: '12SCPFEAST', // Medium (12") Hand Tossed Ultimate Pepperoni
+  code: '14SCPFEAST', // Large (14") Hand Tossed Ultimate Pepperoni
   options: [],
   quantity: 1
 });
 
 order.addItem(pizza);
 
-order.price((price) => {
-  console.log('====PRICE====')
-  console.log(price.result['Order'])
-  console.log('====END PRICE====')
-});
-
-const cardInfo = new order.PaymentObject({
-  Account: order.Amounts.Customer,
-  Number: process.env.CARD_NUMBER,
-  CardType: order.validateCC(process.env.CARD_NUMBER),
-  Expiration: process.env.EXPIRATION, //  01/15 just the numbers "01/15".replace(/\D/g,'');
-  SecurityCode: process.env.SECURITY_CODE,
-  PostalCode: process.env.POSTAL_CODE,
-});
-
-order.Payments.push(cardInfo);
-
 // order.validate((result) => {
 //   console.log('====VALIDATE====')
 //   console.log(result.result['Order'])
 //   console.log('====END VALIDATE====')
 // });
+
+
+order.price((data) => {
+  const amount = data.result['Order'].Amounts.Customer;
+
+  const cardInfo = new order.PaymentObject();
+  cardInfo.Amount = String(amount);
+  cardInfo.Number = process.env.CARD_NUMBER;
+  cardInfo.CardType = order.validateCC(process.env.CARD_NUMBER);
+  cardInfo.Expiration = process.env.EXPIRATION;
+  cardInfo.SecurityCode =  process.env.SECURITY_CODE;
+  cardInfo.PostalCode = process.env.POSTAL_CODE;
+
+  order.Payments.push(cardInfo);
+
+  order.validate((result) => {
+    console.log('====VALIDATE====')
+    console.log(result.result.Order.StatusItems)
+    console.log('====END VALIDATE====')
+  });
+
+});
+
+// console.log('order.Amounts', order.Amounts)
+
+
 
 // inquirer.prompt([{type: 'confirm', name: 'confirm', message: 'place order?'}]).then(
 //   (answer) => {
